d366b77f269b0ff48f428f5e46b239ecae9b15f6
N1ck <cbnickp@gmail.com>
2025-11-13 11:13:25 +0700

    add lang selector before enter website

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,50 +21,48 @@ const getBaseUrl = () => {
 const baseUrl = getBaseUrl();
 
 export const metadata: Metadata = {
-  title: {
-    template: '%s - Local Supply Network',
-    default: 'Local Supply Network - Simplest Way to Buy Weed'
-  },
-  description: 'Local Supply Network is a ...',
-  openGraph: {
-    title: 'Local Supply Network - Simplest Way to Buy Weed',
-    description: 'Local Supply Network is a ...',
-    url: baseUrl,
-    siteName: 'Local Supply Network',
-    images: [
-      {
-        url: `https://cdn.shopify.com/s/files/1/0942/4479/8827/files/lsn-meta-image.jpg?v=1763004994`,
-        width: 1200,
-        height: 630,
-        alt: 'Local Supply Network',
-      },
-    ],
-    locale: 'th_TH',
-    type: 'website',
-  },
-  robots: {
-    index: true,
-    follow: true,
-    "max-image-preview": "large",
-    "max-snippet": -1,
-    "max-video-preview": -1,
-    googleBot: "index, follow"
-  },
-
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Local Supply Network',
-    description: 'Local Supply Network is a ...',
-    images: ['https://cdn.shopify.com/s/files/1/0942/4479/8827/files/lsn-meta-image.jpg?v=1763004994'],
-  },
+  
 }
 
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const locale = params.locale;
   return {
+    title: {
+      template: '%s - Local Supply Network',
+      default: 'Local Supply Network - Simplest Way to Buy Weed'
+    },
+    description: 'Local Supply Network is a ...',
     openGraph: {
+      title: 'Local Supply Network - Simplest Way to Buy Weed',
+      description: 'Local Supply Network is a ...',
+      url: baseUrl,
+      siteName: 'Local Supply Network',
+      images: [
+        {
+          url: `https://cdn.shopify.com/s/files/1/0942/4479/8827/files/lsn-meta-image.jpg?v=1763004994`,
+          width: 1200,
+          height: 630,
+          alt: 'Local Supply Network',
+        },
+      ],
       locale: locale === 'th' ? 'th_TH' : 'en_US',
+      type: 'website',
+    },
+    robots: {
+      index: true,
+      follow: true,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+      "max-video-preview": -1,
+      googleBot: "index, follow"
+    },
+
+    twitter: {
+      card: 'summary_large_image',
+      title: 'Local Supply Network',
+      description: 'Local Supply Network is a ...',
+      images: ['https://cdn.shopify.com/s/files/1/0942/4479/8827/files/lsn-meta-image.jpg?v=1763004994'],
     },
   };
 }
